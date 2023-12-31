9fb2efc3a59ebe89ca38d710e8fe6f000532c788
rabah-usthb <chabanechaoucherabah4@gmail.com>
2023-12-31 06:35:55 -0500

    updated the header_block structure and changed the insertion_block function to allocate for each memory block facteur de blockage element even if there is empty memory left

--- main.c
+++ main.c
@@ -20,6 +20,8 @@ typedef struct file_header{
 
 typedef struct block_header{
     size_t  block_size;
+    int real_nb_block_element;
+    int facteur_blockage;
     char data_type[50];
 }block_header;
 
@@ -30,25 +32,31 @@ printf("preom : %s\n",etudiant.prenom);
 printf("Matricule : %s\n",etudiant.matricule);
 printf("moyenne : %f\n",etudiant.moyenne);
 }
-
 void insertion_block(char* file_path,void* T,int facteur_blockage,size_t size_of_element,int n){
     FILE* file = fopen(file_path,"ab");
     block_header block;
     file_header header;
     strcpy(block.data_type,"Etudiant");
+    block.facteur_blockage = facteur_blockage;
     for(int i = 0;i<n;i=i+facteur_blockage){
-
+        int facteur;
+         if(i+facteur_blockage>n){
+            facteur = facteur_blockage;
+            while(i+facteur > n){
+                --facteur;
+            }
+            block.real_nb_block_element =facteur;
+            }
+        else{
+            facteur = facteur_blockage;
+            block.real_nb_block_element = facteur_blockage;}
         block.block_size = size_of_element *facteur_blockage;
         size_t block_memory_size = sizeof(block_header)+block.block_size;
         void* buffer = malloc(block_memory_size);
         memcpy(buffer,&block,sizeof(block_header));
-        memcpy(buffer+sizeof(block_header),T+i,size_of_element*facteur_blockage);
+        memcpy(buffer+sizeof(block_header),T+i,size_of_element*facteur);
         fwrite(buffer,block_memory_size,1,file);
-    if(i+facteur_blockage>n){
-            while(i+facteur_blockage > n){
-                --facteur_blockage;
-            }
-            }
+
     }
     fclose(file);
 
@@ -61,7 +69,6 @@ void insertion_block(char* file_path,void* T,int facteur_blockage,size_t size_of
     fwrite(&header,sizeof(file_header),1,file_2);
 
 }
-
 
 
 int main(){
