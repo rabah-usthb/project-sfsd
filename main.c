592f74c6ac12b2af85133eb911a3cf5295838cbd
rabah-usthb <chabanechaoucherabah4@gmail.com>
2024-01-01 15:09:54 -0500

    free pointer buffer to avoid memory leak and used one file pointer instead of two in insertion_block function

--- main.c
+++ main.c
@@ -32,12 +32,17 @@ printf("preom : %s\n",etudiant.prenom);
 printf("Matricule : %s\n",etudiant.matricule);
 printf("moyenne : %f\n",etudiant.moyenne);
 }
+
+
+
 void insertion_block(char* file_path,void* T,int facteur_blockage,size_t size_of_element,int n){
-    FILE* file = fopen(file_path,"ab");
+    FILE* file = fopen(file_path,"r+b");
+    fseek(file,0,SEEK_END);
     block_header block;
     file_header header;
     strcpy(block.data_type,"Etudiant");
     block.facteur_blockage = facteur_blockage;
+    void* buffer;
     for(int i = 0;i<n;i=i+facteur_blockage){
         int facteur;
          if(i+facteur_blockage>n){
@@ -52,21 +57,23 @@ void insertion_block(char* file_path,void* T,int facteur_blockage,size_t size_of
             block.real_nb_block_element = facteur_blockage;}
         block.block_size = size_of_element *facteur_blockage;
         size_t block_memory_size = sizeof(block_header)+block.block_size;
-        void* buffer = malloc(block_memory_size);
+         buffer = malloc(block_memory_size);
         memcpy(buffer,&block,sizeof(block_header));
         memcpy(buffer+sizeof(block_header),T+i,size_of_element*facteur);
         fwrite(buffer,block_memory_size,1,file);
 
     }
-    fclose(file);
 
-    FILE* file_2 = fopen(file_path,"r+b");
-    fread(&header,sizeof(file_header),1,file_2);
+    free(buffer);
+
+    fseek(file,0,SEEK_SET);
+    fread(&header,sizeof(file_header),1,file);
     header.nb_element +=n;
-    fseek(file_2,0,SEEK_END);
-    header.file_size = ftell(file_2);
-    fseek(file_2,0,SEEK_SET);
-    fwrite(&header,sizeof(file_header),1,file_2);
+    fseek(file,0,SEEK_END);
+    header.file_size = ftell(file);
+    fseek(file,0,SEEK_SET);
+    fwrite(&header,sizeof(file_header),1,file);
+    fclose(file);
 
 }
 
